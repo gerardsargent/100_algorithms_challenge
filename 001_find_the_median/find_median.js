b08d44b8d636411238dfbd12f7dd6c7079adcc08
Gerard Sargent <gerard.sargent@calligo.cloud>
2019-04-22 12:30:28 +0100

    Initial Commit

---
+++ 001_find_the_median/find_median.js
@@ -0,0 +1,9 @@
+function findMedianValue(array) {
+  array.forEach(function(num) {
+
+  })
+}
+
+console.log(findMedianValue([2, 4]))
+// console.log(findMedianValue([2, 4, 7]))
+// console.log(findMedianValue([2, 4, 7, 9]))
